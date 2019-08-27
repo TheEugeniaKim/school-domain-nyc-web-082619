6ee3856f992ad2f3e3390109ff81838d9dd9bf43
Eugenia Kim <elk303@nyu.edu>
2019-08-27 20:11:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+class School 
+  attr_accessor :student, :grade, :class, :sort, :roster
+  attr_reader :school 
+  
+  
+  def initialize(school)
+    @school = school 
+    @roster = []
+  end
+ 
+  def add_student(student_name, student_grade)
+    @student = student_name
+    @grade = student_grade
+    roster << {student_grade => ["student_name"]}
+  end 
+  
+end
