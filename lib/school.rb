5dc5c3792a7ee368d91ae71f6f2061c9f062ae55
Eugenia Kim <elk303@nyu.edu>
2019-08-27 20:38:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
  
   def add_student(student, grade)
     if @roster[grade] == nil
-      
+      student = [student]
       @roster = {grade => [student]}
       @roster[grade] = [student]
     else 
